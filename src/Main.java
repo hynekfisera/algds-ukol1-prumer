ea96f34ac55e11b65d96f0500cbca88b1e748a2b
Hynek Fišera <hynek.fisera@seznam.cz>
2023-09-27 19:23:46 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getPrumer(0, 5)); // 2.5
+    }
+
+    public static float getPrumer(float a, float b) {
+        return (a + b) / 2;
+    }
+}
